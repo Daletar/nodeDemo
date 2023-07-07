4e87b4224b1c1826e20a83afae6298e8b12ec809
tartar <598589220@qq.com>
2023-07-07 09:25:37 +0800

    2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ "name": "get根目录" });
 });
 
 router.get('/info', function (req, res, next) {
