30f6e76e5d5e9d665ea2576567e434532e9232f3
tartar <598589220@qq.com>
2023-07-07 09:22:11 +0800

    router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/info', function (req, res, next) {
+  res.json({ "name": "tartar" });
 });
 
 module.exports = router;
